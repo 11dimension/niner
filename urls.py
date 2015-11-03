850040b528091eeaa2d1f7256a6f80d691f25604
Arthur.Qin <magus0219@gmail.com>
2015-11-04 07:55:33 +0800

    change url for default repo

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 __author__ = 'magus0219'
 import handlers
 from tornado.web import RedirectHandler
+from core.deploy_manager import dms
 
 URLS = [
             # Core Entity
@@ -8,7 +9,7 @@ URLS = [
             (r'/repo/(.*)/rollback/(.*)/(.*)', handlers.RollbackHandler),
             (r'/repo/(.*)/(.*)', handlers.OperationHandler),
             (r'/repo/(.*)', handlers.IndexHandler),
-            (r'/repo', RedirectHandler, {"url": "/deploy/repo/zqhua_server"}),
+            (r'/repo', RedirectHandler, {"url": "/deploy/repo/{repo}".format(repo=dms[0])}),
             (r'/login', handlers.LoginHandler),
             (r'/logout', handlers.LogoutHandler),
             (r'/register', handlers.RegisterHandler),
